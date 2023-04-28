0af04b1a476648c39605c833adf92d2f45a9510a
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-28 15:09:54 +0600

    Business Tax in JavaScript

---
+++ businessTax.js
@@ -0,0 +1,6 @@
+let sales = 95000;
+let stateTaxRate = 0.04;
+let stateTax = sales * stateTaxRate;
+let countryTaxRate = 0.02;
+let countryTax = sales * countryTaxRate;
+document.write("Sales = " + sales + "</br>" + " State Tax = " + stateTax + "</br>" + " Country Tax = " + countryTax);
